11cc22d861b43235df4abac9b9308a157900f0fa
bonil100 <bonil100@mail.chapman.edu>
2016-11-28 00:07:30 -0800

    updated search

--- green.java
+++ green.java
@@ -38,6 +38,52 @@ public class green
             System.exit(-1);
         }
 
+        //search queries related to climate change 
+        //api allows search of 100 tweets at a time.
+        System.out.println("Showing tweets related to the search: climate change.");
+        System.out.println(" ");
+        try
+        {
+            Query query = new Query("climatechange");
+            QueryResult result;
+            result = twitter.search(query);
+            List<Status> status = result.getTweets();
+            for (Status s : status)
+            {
+                System.out.println("@" + s.getUser().getName() + ":" + s.getText());
+            }
+        }
+        
+        catch (TwitterException te)
+        {
+            te.printStackTrace();
+            System.out.println("Error: " + te.getMessage());
+            System.exit(-1);
+        }
+
+        System.out.println(" ");
+        System.out.println("The following are public tweets related to the search: globalwarming.");
+        System.out.println(" ");
+        try
+        {
+            Query query = new Query("globalwarming");
+            QueryResult result;
+            result = twitter.search(query);
+            List<Status> status = result.getTweets();
+            for (Status s : status)
+            {
+                System.out.println("@" + s.getUser().getName() + ":" + s.getText());
+            }
+        }
+        
+        catch (TwitterException te)
+        {
+            te.printStackTrace();
+            System.out.println("Error: " + te.getMessage());
+            System.exit(-1);
+        }
+
+
 	}
 
 }
