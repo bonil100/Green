0e73943e516af11d522b4246e41e98f2c9c62855
bonil100 <bonil100@mail.chapman.edu>
2016-11-29 23:12:36 -0800

    making it neater

--- green.java
+++ green.java
@@ -1,14 +1,16 @@
 import twitter4j.conf.ConfigurationBuilder;
 import twitter4j.*;
 import java.util.List;
+import java.util.Scanner;
 
 public class green
 {
 	public static void main(String[] args)
-	{
+	{ 
+
         //authentication keys and tokens
-		ConfigurationBuilder cb = new ConfigurationBuilder();
-		cb.setDebugEnabled(true)
+        ConfigurationBuilder cb = new ConfigurationBuilder();
+        cb.setDebugEnabled(true)
             .setOAuthConsumerKey("J4esNudphsCsCki7L3NNg3dxx")
             .setOAuthConsumerSecret("Z2UCKb3JWL3jRy1PGTPYuzPDFboF8jJnkQeTlXQLVg8DP7xPjq")
             .setOAuthAccessToken("775047840250732544-9F3LQWH9czrpVJzd9Ab3sXbSjXxDaBy")
@@ -17,70 +19,85 @@ public class green
         //get new twitter instance    
         TwitterFactory tf = new TwitterFactory(cb.build());
         Twitter twitter = tf.getInstance();
-        
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("How do you want to search for climate change news?" + "\n");
+        System.out.println("Enter 'user' to search popular environmental users." + "\n");
+        System.out.println("Enter 'search' to search by environmental related queries" + "\n");
+
+        String response = input.nextLine();
+
+
         //get home timeline
-        //my account follows top 5 environmental accounts 
-        //this shows those accounts posts on my home timeline
-        try
+        //my account follows only environmental accounts--> showing environmental news posted by popular environmental accounts
+        //this shows tweets by popular environmental users on my home timeline
+        if (response.equals("user"))
         {
-            List<Status> status = twitter.getHomeTimeline();
-            System.out.println("Showing home timeline.");
-            for (Status s : status)
+            try
             {
-                System.out.println(s.getUser().getName() + ":" + s.getText());
+                List<Status> status = twitter.getHomeTimeline();
+                System.out.println("Showing home timeline.");
+                for (Status s : status)
+                {
+                    System.out.println(s.getUser().getName() + ":" + s.getText() + "\n");
+                }
             }
+        
+            catch (TwitterException te)
+            {
+                te.printStackTrace();
+                System.out.println("Error: " + te.getMessage());
+                System.exit(-1);
+            }
+    
         }
         
-        catch (TwitterException te)
-        {
-            te.printStackTrace();
-            System.out.println("Error: " + te.getMessage());
-            System.exit(-1);
-        }
-
         //search queries related to climate change 
-        //api allows search of 100 tweets at a time.
-        System.out.println("Showing tweets related to the search: climate change.");
-        System.out.println(" ");
-        try
+        //twitter api allows search of 100 tweets at a time.
+        else if (response.equals("search"))
         {
-            Query query = new Query("climatechange");
-            QueryResult result;
-            result = twitter.search(query);
-            List<Status> status = result.getTweets();
-            for (Status s : status)
+            System.out.println("Showing tweets related to the search: climate change.");
+            System.out.println(" ");
+            try
             {
-                System.out.println("@" + s.getUser().getName() + ":" + s.getText());
+                Query query = new Query("climatechange");
+                QueryResult result;
+                result = twitter.search(query);
+                List<Status> status = result.getTweets();
+                for (Status s : status)
+                {
+                System.out.println("@" + s.getUser().getName() + ":" + s.getText() + "\n");
+                }
             }
-        }
         
-        catch (TwitterException te)
-        {
-            te.printStackTrace();
-            System.out.println("Error: " + te.getMessage());
-            System.exit(-1);
-        }
+            catch (TwitterException te)
+            {
+                te.printStackTrace();
+                System.out.println("Error: " + te.getMessage());
+                System.exit(-1);
+            }
 
-        System.out.println(" ");
-        System.out.println("The following are public tweets related to the search: globalwarming.");
-        System.out.println(" ");
-        try
-        {
-            Query query = new Query("globalwarming");
-            QueryResult result;
-            result = twitter.search(query);
-            List<Status> status = result.getTweets();
-            for (Status s : status)
+            System.out.println(" ");
+            System.out.println("The following are public tweets related to the search: globalwarming.");
+            System.out.println(" ");
+            try
             {
-                System.out.println("@" + s.getUser().getName() + ":" + s.getText());
+                Query query = new Query("globalwarming");
+                QueryResult result;
+                result = twitter.search(query);
+                List<Status> status = result.getTweets();
+                for (Status s : status)
+                {
+                    System.out.println("@" + s.getUser().getName() + ":" + s.getText() + "\n");
+                }
             }
-        }
         
-        catch (TwitterException te)
-        {
-            te.printStackTrace();
-            System.out.println("Error: " + te.getMessage());
-            System.exit(-1);
+            catch (TwitterException te)
+            {
+                te.printStackTrace();
+                System.out.println("Error: " + te.getMessage());
+                System.exit(-1);
+            }
         }
 
 
